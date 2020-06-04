fdb87f90403e47237fe7d7458b28fc302219e9af
Tulio Braz <tuliogbraz@gmail.com>
2020-06-04 18:28:13 -0300

    Comi cu de curioso

---
+++ Gitgay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gitgay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eae! Aqui é o Gitgayson");
+        }
+    }
+}
